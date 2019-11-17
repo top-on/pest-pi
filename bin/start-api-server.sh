02ff3976168e6e1d20a5d1ff6862277f1a734736
Thorben Jensen <jensen.thorben@gmail.com>
2019-11-17 18:03:37 +0100

    move script to start server and link in setup.py

--- bin/start-api-server.sh
+++ bin/start-api-server.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # start API server for image classification
 
-python -m uvicorn src.api_classifier:app --reload
+python -m uvicorn pest_pi.api_classifier:app --reload
--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ from setuptools import setup
 
 
 # Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
@@ -21,6 +18,7 @@ setup(
     keywords="raspberry pi tensorflow",
     url="https://github.com/thorbenJensen/pest-pi",
     packages=["pest_pi"],
+    scripts=['bin/start-api-server.sh'],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
